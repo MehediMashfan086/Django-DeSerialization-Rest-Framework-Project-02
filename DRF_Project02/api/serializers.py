688ddc38c02022aacd6a0867292b6751ab3f43b9
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-11-28 01:34:37 +0600

    Saving to GitHub

--- DRF_Project02/api/serializers.py
+++ DRF_Project02/api/serializers.py
@@ -5,3 +5,6 @@ class StudentSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100)
     roll = serializers.IntegerField()
     city = serializers.CharField(max_length=100)
+
+    def create(self, validate_data):
+        return Student.objects.create(**validate_data)
